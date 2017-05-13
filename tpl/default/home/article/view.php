cfb503f996a8692740eeadee27ef16f8ebb5face
xuerhuo <824851582@qq.com>
2017-05-13 15:58:29 +0800

    tpl

--- tpl/default/home/article/view.php
+++ tpl/default/home/article/view.php
@@ -9,6 +9,7 @@
     <?= import('reset.css') ?>
     <?= import('common.css') ?>
     <?= import('common.js') ?>
+    <?= import('home_article_view.css') ?>
     <?= import('jquery-2.1.0.js') ?>
     <script src="<?php echo DATA_LIB ?>editor/editormd.js"></script>
     <script src="<?php echo DATA_LIB ?>editor/lib/marked.min.js"></script>
