7f6a82959104cfe2bae0763e3175f829c125932a
xuerhuo <824851582@qq.com>
2017-06-12 11:40:30 +0800

    cnzz count

--- home/controller/article/view.articleController.php
+++ home/controller/article/view.articleController.php
@@ -1,12 +1,12 @@
 <?php
-$article = M('\Cms\admin\article')->getArticles(array('id' => $G['get']['param']['aid']))[0];
+$article = M('\Cms\admin\article')->getArticles(array('id' => $G['get']['param']['aid']), 1)[0];
 $aaroundarticle = M('\Cms\admin\article')->getAroundArticles($article['dateline']);
 $comments = M('Cms\home\comments')->getCommentsById($G['get']['param']['aid'], 'article');
 $comments = array_diyfiter($comments, 'html');
 if ($G['get']['param']['ajax'] == 1) {
     json_output($article['content'], 'text');
 }
-$article['content'] = htmlspecialchars($article['content']);
+//$article['content'] = htmlspecialchars($article['content']);
 $title = $article['title'] . ' - ';
 $site_keywords = $article['tags'];
 $site_description = $article['title'];
--- tpl/default/include/common_footer.php
+++ tpl/default/include/common_footer.php
@@ -1,5 +1,13 @@
 <div class="cl"></div>
-</div>
 
+
+</div>
+<div class="foooter">
+    <div class="tj">
+        <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+            document.write(unescape("%3Cspan id='cnzz_stat_icon_1262217463'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s13.cnzz.com/z_stat.php%3Fid%3D1262217463%26online%3D1%26show%3Dline' type='text/javascript'%3E%3C/script%3E"));</script>
+    </div>
+
+</div>
 </body>
 </html>
