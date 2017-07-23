13b1e2751f2f6de416373510eb699c3cf74c687b
xuerhuo <824851582@qq.com>
2017-07-23 18:46:10 +0800

    修改通用图片显示js

--- data/app.conf.php
+++ data/app.conf.php
@@ -2,7 +2,7 @@
 $config['tpl'] = ROOT . 'tpl' . DIRECTORY_SEPARATOR . 'default' . DIRECTORY_SEPARATOR;
 $config['croncalltime'] = 60;
 $config['siteurl'] = 'http://erhuo.org/';
-$config['debug'] = false;
+$config['debug'] = true;
 $config['allow_upload_type'] = array('jpg', 'jpeg', 'gif', 'bmp', 'psd', 'png', 'apk', 'rar', 'css', 'txt', 'gz', 'tar');
 $config['error_level'] = 0;
 $config['cache_enable'] = true;
--- tpl/default/static/js/admin_menu_curdlist.js
+++ tpl/default/static/js/admin_menu_curdlist.js
@@ -1,20 +1,71 @@
-$(document).ready(function () {
-    jQuery(".showimg").hover(function () {
-        var position = jQuery(this).position().left + 55;
-        var positiont = jQuery("table").width() / 2;
-        if (position <= positiont) {
-            jQuery(".topimg").css("left", position + "px");
-            jQuery(".topimg").show();
-            var imgUrl = jQuery(this)[0].src;
-            jQuery(".topimg img").attr('src', imgUrl);
-        } else {
-            jQuery(".topimg").css({"left": position + "px", "margin-left": "-660px"});
-            jQuery(".topimg").show();
-            var imgUrl = jQuery(this)[0].src;
-            jQuery(".topimg img").attr('src', imgUrl);
-        }
-    }, function () {
-        jQuery(".topimg").hide();
-        jQuery(".topimg img").attr('src', '');
+// $(document).ready(function () {
+//     jQuery(".showimg").hover(function () {
+//         var position = jQuery(this).position().left + 55;
+//         var positiont = jQuery("table").width() / 2;
+//         if (position <= positiont) {
+//             jQuery(".topimg").css("left", position + "px");
+//             jQuery(".topimg").show();
+//             var imgUrl = jQuery(this)[0].src;
+//             jQuery(".topimg img").attr('src', imgUrl);
+//         } else {
+//             jQuery(".topimg").css({"left": position + "px", "margin-left": "-660px"});
+//             jQuery(".topimg").show();
+//             var imgUrl = jQuery(this)[0].src;
+//             jQuery(".topimg img").attr('src', imgUrl);
+//         }
+//     }, function () {
+//         jQuery(".topimg").hide();
+//         jQuery(".topimg img").attr('src', '');
+//     })
+// });
+// $(document).ready(function () {
+//
+// });
+window.onload = function () {
+    addEvent(".showimg", "click", imgshow);
+}
+
+function imgshow() {
+    var divNode = document.createElement("div");
+    divNode.style.cssText = "    top: 0px;\
+    bottom: 0px;\
+    left: 0px;\
+    right: 0px;\
+    background-color: rgb(0, 0, 0);\
+    position: fixed;\
+    opacity: 0.93;";
+    var bodyNode = document.querySelector("body");
+
+    var divNode2 = document.createElement("div");
+    divNode2.style.cssText = "\
+    background-color:#000000\
+    display:inline-block;\
+    margin: 0 auto;\
+    text-align:center;\
+    width:100%;\
+    max-height:80%;\
+    ";
+
+    var imgNode = document.createElement("img");
+    imgNode.src = this.src;
+    imgNode.style.cssText = "height: 100%;\
+    display: inline-block;";
+
+    divNode2.appendChild(imgNode);
+
+    divNode.appendChild(divNode2);
+    bodyNode.appendChild(divNode);
+
+    divNode.addEventListener("click", function () {
+        bodyNode.removeChild(divNode);
     })
-});
+
+}
+
+function buquanpic(obj, url) {
+    console.log(obj);
+    var src = obj.src;
+    console.log(url);
+    obj.src = site_url + "/data/file/" + url;
+    console.log(obj.src);
+}
