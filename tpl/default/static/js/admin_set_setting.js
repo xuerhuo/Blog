4c29bc225895f0ecf6ce56b93e50d35234b6b377
xuerhuo <824851582@qq.com>
2017-07-27 23:10:54 +0800

    init

--- tpl/default/static/js/admin_set_setting.js
+++ tpl/default/static/js/admin_set_setting.js
@@ -1,9 +1,43 @@
 /**
  * Created by erhuo on 2017/2/17.
  */
+
 window.onload = function () {
+    var inputName = '';
+    var _editor = '';
     addEvent(".wrap .title .c-menu a", "click", menuclick);
     addEvent(".wrap #c-menu-addsetting .bottom .post", "click", addSetting);
+    initueditor();
+}
+
+function openuploadframe(name) {
+    inputName = name;
+    _editor.getDialog("insertimage").open();
+}
+
+function initueditor() {
+    _editor = UE.getEditor('ueditor');
+    _editor.ready(function () {
+
+        _editor.setDisabled();
+        _editor.hide();
+        _editor.addListener("beforeinsertimage", function (t, arg) {
+            uploadcallback(arg);
+            console.log(t, arg);
+            // console.log(JSON.stringify(arg));
+        });
+        _editor.addListener("afterupfile", function (t, arg) {
+            uploadcallback(arg);
+            console.log(t, arg);
+            // console.log(JSON.stringify(arg));
+        });
+    });
+}
+
+function uploadcallback(obj) {
+    var input = document.getElementsByName(inputName)[0];
+    input.value = obj[0].src;
+    alert("上传成功");
 }
 function menuclick() {
     if (document.querySelector("#c-menu-addsetting").style.display == "none") {
