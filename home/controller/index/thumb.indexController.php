0028b21cebb413f9942db8b49838790d1973e02e
xuerhuo <824851582@qq.com>
2017-08-10 16:59:53 +0800

    图片压缩

--- home/controller/index/thumb.indexController.php
+++ home/controller/index/thumb.indexController.php
@@ -5,5 +5,5 @@
  * Date: 2017/7/22
  * Time: 10:26
  */
-$path = base64_decode($G['get']['param']['path']);
+$path = ROOT.base64_decode($G['get']['param']['path']);
 $temp = new \Cms\common\imgcompress($path,0.1);
